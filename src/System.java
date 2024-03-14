a6fd9f79ce387819f37a3188817d4ce0952c85a5
Alo080701 <rosette.louise@hotmail.dk>
2024-03-14 12:22:53 +0100

    SKUB SKUB SKUB

--- src/System.java
+++ src/System.java
@@ -19,45 +19,6 @@ public class System {
     static Statement stmt;
     static BufferedReader inLine;
 
-    public static void selectudenparm() {
-        try {
-            // Laver sql-sætning og får den udført
-            String sql = "select navn,stilling from person";
-            java.lang.System.out.println("SQL-streng er " + sql);
-            ResultSet res = stmt.executeQuery(sql);
-            // gennemløber svaret
-            while (res.next()) {
-                String s;
-                s = res.getString("navn");
-                java.lang.System.out.println(s + "    " + res.getString(2));
-            }
-            // pæn lukning
-            if (!minConnection.isClosed()) minConnection.close();
-        } catch (Exception e) {
-            java.lang.System.out.println("fejl:  " + e.getMessage());
-        }
-    }
-
-    public static void selectmedparm() {
-        try {
-            // Indlæser søgestreng
-            java.lang.System.out.println("Indtast søgestreng");
-            String inString = inLine.readLine();
-            // Laver sql-sætning og får den udført
-            String sql = "select navn,stilling from person where navn like '" + inString + "%'";
-            java.lang.System.out.println("SQL-streng er " + sql);
-            ResultSet res = stmt.executeQuery(sql);
-            //gennemløber svaret
-            while (res.next()) {
-                java.lang.System.out.println(res.getString(1) + "    " + res.getString(2));
-            }
-            // pæn lukning
-            if (!minConnection.isClosed()) minConnection.close();
-        } catch (Exception e) {
-            java.lang.System.out.println("fejl:  " + e.getMessage());
-        }
-    }
-
     public static void insertmedstring() {
         try {
             // indlæsning
@@ -102,126 +63,26 @@ public class System {
     }
 
 
-    public static void insertprepared() {
-        try {
-            // indl�sning
-            java.lang.System.out.println("Vi vil nu oprette et nyt ansættelsesforhold");
-            java.lang.System.out.println("Indtast cpr (personen skal være oprettet på forhånd");
-            String cprstr = inLine.readLine();
-            java.lang.System.out.println("Indtast firmanr (firma skal være oprettet på forhånd");
-            String firmastr = inLine.readLine();
-            // Anvendelse af prepared statement
-            String sql = "insert into ansati values (?,?)";
-            PreparedStatement prestmt = minConnection.prepareStatement(sql);
-            prestmt.clearParameters();
-            prestmt.setString(1, cprstr);
-            prestmt.setInt(2, Integer.parseInt(firmastr));
-            // Udf�rer s�tningen
-            prestmt.execute();
-            // p�nt svar til brugeren
-            java.lang.System.out.println("Ansættelsen er nu registreret");
-            if (!minConnection.isClosed()) minConnection.close();
-        } catch (SQLException e) {
-            switch (e.getErrorCode())
-            // fejl-kode 547 svarer til en foreign key fejl
-            {
-                case 547: {
-                    if (e.getMessage().contains("cprforeign"))
-                        java.lang.System.out.println("cpr-nummer er ikke oprettet");
-                    else if (e.getMessage().contains("firmaforeign"))
-                        java.lang.System.out.println("firmaet er ikke oprettet");
-                    else
-                        java.lang.System.out.println("ukendt fremmednøglefejl");
-                    break;
-                }
-                // fejl-kode 2627 svarer til primary key fejl
-                case 2627: {
-                    java.lang.System.out.println("den pågældende ansættelse er allerede oprettet");
-                    break;
-                }
-                default:
-                    java.lang.System.out.println("fejlSQL:  " + e.getMessage());
-            }
-            ;
-        } catch (Exception e) {
-            java.lang.System.out.println("fejl:  " + e.getMessage());
-        }
-    }
 
-    public static void selectmedpis() {
-        try {
-            // Laver sql-sætning og får den udført
-            String sql = "select navn, stilling, postdistrikt, firmanavn from opstilling";
-            java.lang.System.out.println("SQL-streng er " + sql);
-            ResultSet res = stmt.executeQuery(sql);
-            // gennemløber svaret
-            while (res.next()) {
-                String s;
-                s = res.getString("navn");
-                java.lang.System.out.println(s + "  [" + res.getString(2) + "], Postdistrikt: " + res.getString(3) + ", arbejdsplads: " + res.getString(4));
-            }
-            // pæn lukning
-            if (!minConnection.isClosed()) minConnection.close();
-        } catch (Exception e) {
-            java.lang.System.out.println("fejl:  " + e.getMessage());
-        }
-    }
-
-    public static void selectmedost() {
-        try {
-            // Laver sql-sætning og får den udført
-            String sql = "select navn from person where loen > (select AVG(loen) from person)";
-            java.lang.System.out.println("SQL-streng er " + sql);
-            ResultSet res = stmt.executeQuery(sql);
-            // gennemløber svaret
-            while (res.next()) {
-                String s;
-                s = res.getString("navn");
-                java.lang.System.out.println(s);
-            }
-            // pæn lukning
-            if (!minConnection.isClosed()) minConnection.close();
-        } catch (Exception e) {
-            java.lang.System.out.println("fejl:  " + e.getMessage());
-        }
-    }
-
-    public static void selectmedbjerg() {
-        try {
-            // Laver sql-sætning og får den udført
-            String sql = "select stilling, navn from person where loen in (select MAX(loen) as maxløn from person group by stilling);";
-            java.lang.System.out.println("SQL-streng er " + sql);
-            ResultSet res = stmt.executeQuery(sql);
-            // gennemløber svaret
-            while (res.next()) {
-                String s;
-                s = res.getString("stilling");
-                java.lang.System.out.println(s + " " + res.getString(2));
-            }
-            // pæn lukning
-            if (!minConnection.isClosed()) minConnection.close();
-        } catch (Exception e) {
-            java.lang.System.out.println("fejl:  " + e.getMessage());
-        }
-    }
-
-    public static void insertPerson() {
+    public static void InsertEksamensafvikling() {
         try {
             // indlæsning
-            java.lang.System.out.println("Vi vil nu oprette en nyperson");
-            java.lang.System.out.println("Indtast cpr");
-            String cprstr = inLine.readLine();
-            java.lang.System.out.println("Indtast navn");
-            String navnstr = inLine.readLine();
-            java.lang.System.out.println("Indtast stilling");
-            String stillingstr = inLine.readLine();
-            java.lang.System.out.println("Indtast løn");
-            String loenstr = inLine.readLine();
-            java.lang.System.out.println("Indtast postnummer");
-            String pnstr = inLine.readLine();
+            java.lang.System.out.println("Vi vil nu oprette en ny Eksamensafvikling");
+            java.lang.System.out.println("Indtast id");
+            String iDstr = inLine.readLine();
+            java.lang.System.out.println("Indtast er stopprøve (0 = false og 1 = true)");
+            String sPstr = inLine.readLine();
+            java.lang.System.out.println("Indtast startDato (År-Måned-Dag)");
+            String startDstr = inLine.readLine();
+            java.lang.System.out.println("Indtast slutDato (År-Måned-Dag)");
+            String slutDstr = inLine.readLine();
+            java.lang.System.out.println("Indtast Termin (S=Sommer og V=Vinter, EKS. S2024) ");
+            String tstr = inLine.readLine();
+            java.lang.System.out.println("Indtast eksamens nr");
+            String eksNrstr = inLine.readLine();
 
             // sender insert'en til db-serveren
-            String sql = "insert into person values ('" + cprstr + "', '" + navnstr + "', '" + stillingstr + "', '" + loenstr + "', '" + pnstr + "')";
+            String sql = "insert into person values ()";
             java.lang.System.out.println("SQL-streng er " + sql);
             stmt.execute(sql);
             // pænt svar til brugeren
@@ -254,46 +115,6 @@ public class System {
         }
     }
 
-    public static void insertpostnummer() {
-        try {
-            // indlæsning
-            java.lang.System.out.println("Vi vil nu oprette en nyperson");
-            java.lang.System.out.println("Indtast postnummer");
-            String postnrstr = inLine.readLine();
-            java.lang.System.out.println("Indtast postdistrikt");
-            String postdistr = inLine.readLine();
-
-            // sender insert'en til db-serveren
-            String sql = "insert into postnummer values ('" + postnrstr + "', '" + postdistr + "')";
-            java.lang.System.out.println("SQL-streng er " + sql);
-            stmt.execute(sql);
-            // pænt svar til brugeren
-            java.lang.System.out.println("postnummer er nu registreret");
-            if (!minConnection.isClosed()) minConnection.close();
-        } catch (SQLException e) {
-            switch (e.getErrorCode())
-            // fejl-kode 547 svarer til en foreign key fejl
-            {
-                case 547: {
-                    if (e.getMessage().contains("postnrforeign"))
-                        java.lang.System.out.println("postnummer er ikke oprettet");
-                    else
-                        java.lang.System.out.println("ukendt fremmednøglefejl");
-                    break;
-                }
-                // fejl-kode 2627 svarer til primary key fejl
-                case 2627: {
-                    java.lang.System.out.println("fejl med primary key");
-                    break;
-                }
-                default:
-                    java.lang.System.out.println("fejlSQL:  " + e.getMessage());
-            }
-            ;
-        } catch (Exception e) {
-            java.lang.System.out.println("fejl:  " + e.getMessage());
-        }
-    }
 
     public static void main(String[] args) {
         // TODO Auto-generated method stub
@@ -325,7 +146,7 @@ public class System {
                     break;
                 }
                 case "B": {
-                    selectmedparm();
+                    InsertEksamensafvikling();
                     break;
                 }
                 case "C": {
